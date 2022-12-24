830ba0b962da374f807898bf9b38af2cda9d5bbf
jootacee <jootacee3@gmail.com>
2022-12-23 22:52:21 -0300

    new fe

---
+++ js/quiz.js
@@ -0,0 +1,9 @@
+const form = document.querySelector('#form')
+const btnSubmit = document.querySelector('.submit')
+
+const userAnswer = document.querySelector('input[name="q1"]:checked').value
+
+btnSubmit.addEventListener('click', (e) => {
+    e.preventDefault()
+    console.log(userAnswer)
+})
