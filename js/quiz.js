f0fef9fe9e57e0c4ea2e379aede345c64e7b86e8
jootacee <jootacee3@gmail.com>
2022-12-25 10:36:56 -0300

    :sparkles: new feature

--- js/quiz.js
+++ js/quiz.js
@@ -1,9 +1,17 @@
 const form = document.querySelector('#form')
-const btnSubmit = document.querySelector('.submit')
+const btnSubmit = document.querySelector('.myButton')
 
-const userAnswer = document.querySelector('input[name="q1"]:checked').value
+const anwser = {name: "q1", correctAnwser: "Inglaterra"}
 
 btnSubmit.addEventListener('click', (e) => {
-    e.preventDefault()
-    console.log(userAnswer)
+    e.preventDefault();
+    const response = document.createElement("p")
+    const userAnwser = form.q1.value
+    if (userAnwser != anwser.correctAnwser) {
+      response.innerHTML = '<h1>Resposta errada!</h1>'
+      document.body.appendChild(response)
+    } else {
+      response.innerHTML = '<h1>Resposta correta!</h1>'
+      document.body.appendChild(response)
+    }
 })
